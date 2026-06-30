baa8980095b2c062ddce0914e7c860ad2984a7ce
Belardino-Capessa <belardinocapessa@gmail.com>
2026-06-30 05:48:09 +0100

    Add service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = "ludochaos-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./assets/fontawesome/css/all.min.css",
+  "./assets/icons/logo192.png",
+  "./assets/icons/logo512.png"
+];
+
+// Instalação
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Ativação
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+// Interceptar requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
